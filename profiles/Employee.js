416dbf207be2f11d394f489bec443db45fd5278c
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:20:59 +0100

    Employee parent constructor

---
+++ profiles/Employee.js
@@ -0,0 +1,7 @@
+// employee parent constructor 
+class Employee {
+    constructor (preferredName, idNumber, emailAddress) {
+        this.preferredName = preferredName;
+        this.idNumber = idNumber;
+        this.emailAddress = emailAddress;
+    }
