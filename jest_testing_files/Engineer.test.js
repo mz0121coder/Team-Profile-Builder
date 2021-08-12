68065ece8137ca1817bc43a98f865e1382ed9587
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:14:14 +0100

    Engineer dependency + object example

---
+++ jest_testing_files/Engineer.test.js
@@ -0,0 +1,6 @@
+// const engineer requires engineer profile
+const Engineer = require('../profiles/Engineer');
+
+// create object representing engineer
+test('create object representing engineer', () => {
+    const engineer = new Engineer('Mohammad', 2, 'mzcoder7@gmail.com');
