0306688592623b0715e52904e9935a1216a1e765
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:19:31 +0100

    Manager office number test info

--- jest_testing_files/Manager.test.js
+++ jest_testing_files/Manager.test.js
@@ -4,4 +4,6 @@ const Manager = require('../profiles/Manager');
 // creating object respresenting manager 
 test('create an object representing the manager', () => {
     const manager = new Manager('Mohammad', 2, 'mzcoder7@gmail.com');
-    
+// expect office number test to result in a number value   
+expect(manager.officeNumber).toEqual(expect.any(Number));
+});
