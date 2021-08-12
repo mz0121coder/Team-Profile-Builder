f5aa741e5aed1b38d90c7a1b2f28a2718a0a1c75
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:11:31 +0100

    Employee dependency + object test info

---
+++ jest_testing_files/Employee.test.js
@@ -0,0 +1,11 @@
+// employee const requires Employee profile
+const Employee = require('../profiles/Employee');
+
+// employee object test
+test('create an object representing the employee', () => {
+    const employee = new Employee('Mohammad', 2, 'mzcoder7@gmail.com');
+//expect name and email to equal string, expect id to equal a number
+    expect(employee.preferredName).toEqual(expect.any(String));
+    expect(employee.idNumber).toEqual(expect.any(Number));
+    expect(employee.emailAddress).toEqual(expect.any(String));
+});
