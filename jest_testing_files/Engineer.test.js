b1efa0e8a2e9813d6e31b5ed2cf5416c2491434d
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:14:43 +0100

    Engineer GitHub info test

--- jest_testing_files/Engineer.test.js
+++ jest_testing_files/Engineer.test.js
@@ -4,3 +4,13 @@ const Engineer = require('../profiles/Engineer');
 // create object representing engineer
 test('create object representing engineer', () => {
     const engineer = new Engineer('Mohammad', 2, 'mzcoder7@gmail.com');
+// expect github name to equal a string   
+expect(engineer.gitHubName).toEqual(expect.any(String));
+});
+
+// get github from getGithub()
+test('get github profile of engineer', () => {
+    const engineer = new Engineer('Mohammad', 2, 'mzcoder7@gmail.com');
+// expect getGithub() to return string containing engineer's github name
+    expect(engineer.getGitHubName()).toEqual(expect.stringContaining(engineer.gitHubName.toString()));
+});
