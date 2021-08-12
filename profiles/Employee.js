2aa554a6b5249eeaeed7fb406835c954fa22bec3
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:21:56 +0100

    Get employee ID, email, occupation

--- profiles/Employee.js
+++ profiles/Employee.js
@@ -9,3 +9,16 @@ class Employee {
 getPreferredName() {
     return this.preferredName;
 }
+// get ID number
+getIdNumber() {
+    return this.idNumber;
+}   
+// get Email address
+getEmailAddress() {
+    return this.emailAddress;
+}
+// get Occupation = "Employee"
+getOccupation () {
+    return 'Employee'; 
+}
+};
