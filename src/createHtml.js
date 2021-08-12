bc6c87f3c3ac5a0aba4d405f76fa54f5dc9531b5
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:00:41 +0100

    Create engineer information

--- src/createHtml.js
+++ src/createHtml.js
@@ -18,3 +18,24 @@ const createManager = function(manager) {
     </div>
     `;
 }
+
+// create engineer information
+const createEngineer = function(engineer) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="info-header">
+                <h3>${engineer.preferredName}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+
+            <div class="info">
+                <p class="idnumber">ID: ${engineer.idNumber}</p>
+                <p class="emailaddress">Email: <a href="mailto:${engineer.emailAddress}">${engineer.emailAddress}</a></p>
+                <p class="githubname">Github: <a href="https://github.com/${engineer.gitHubName}">${engineer.gitHubName}</a></p>
+            </div>
+
+        </div>
+    </div>
+    `
+}
