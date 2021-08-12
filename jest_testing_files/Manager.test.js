b200ccfa3722c61b402283581631ab3129d3242f
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:20:01 +0100

    Expect role to be "Manager"

--- jest_testing_files/Manager.test.js
+++ jest_testing_files/Manager.test.js
@@ -7,3 +7,9 @@ test('create an object representing the manager', () => {
 // expect office number test to result in a number value   
 expect(manager.officeNumber).toEqual(expect.any(Number));
 });
+// manager job testing
+test('get current job of manager', () => {
+    const manager = new Manager('Mohammad', 2, 'mzcoder7@gmail.com');
+// expect job = "Manager"
+    expect(manager.getOccupation()).toEqual("Manager");
+}); 
