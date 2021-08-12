c58275867de1ec2fc510afb9db59b0e650758361
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:16:30 +0100

    Intern school name testing

--- jest_testing_files/Intern.test.js
+++ jest_testing_files/Intern.test.js
@@ -4,3 +4,13 @@ const Intern = require('../profiles/Intern');
 // create object representing intern
 test('create object representing intern', () => {
     const intern = new Intern('Mohammad', 2, 'mzcoder7@gmail.com', "2U");
+ // expect school name to equal a string   
+ expect(intern.schoolName).toEqual(expect.any(String));
+});
+
+// get school information from getEducationInfo()
+test('get name of school being attended by intern', () => {
+    const intern = new Intern('Mohammad', 2, 'mzcoder7@gmail.com', "2U");
+// expect test to return a string containing name of school attended
+    expect(intern.getEducationInfo()).toEqual(expect.stringContaining(intern.schoolName.toString()));
+});
