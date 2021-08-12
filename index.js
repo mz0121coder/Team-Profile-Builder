684fc98039a6ca3982a8eeaf35bf3c2a82621e14
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 01:53:06 +0100

    Manager/Engineer/Intern profiles required

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // link to create HTML page
 const createHTML = require('./src/createHtml');
+
+// Manager/Engineer/Intern profiles required 
+const Manager = require('./profiles/Manager');
+const Engineer = require('./profiles/Engineer');
+const Intern = require('./profiles/Intern'); 
