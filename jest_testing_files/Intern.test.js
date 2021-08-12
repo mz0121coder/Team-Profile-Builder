fd9a3a189eaa8c5bdb78a548ab78801fe0a2c793
Mohammad Zafar <sz7861@outlook.com>
2021-08-12 02:15:58 +0100

    Intern dependency + object info

---
+++ jest_testing_files/Intern.test.js
@@ -0,0 +1,6 @@
+// intern const requires intern profile
+const Intern = require('../profiles/Intern');
+
+// create object representing intern
+test('create object representing intern', () => {
+    const intern = new Intern('Mohammad', 2, 'mzcoder7@gmail.com', "2U");
